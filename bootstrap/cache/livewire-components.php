1e1b61c3da7638d6f5bd12645e7adbe37aa52ed6
Adugna <gizawadugna@gmail.com>
2025-04-01 12:03:53 +0300

    initial

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,7 @@
+<?php return array (
+  'category-edit-form' => 'App\\Http\\Livewire\\CategoryEditForm',
+  'custom-field-set-default-values-for-model' => 'App\\Http\\Livewire\\CustomFieldSetDefaultValuesForModel',
+  'importer' => 'App\\Http\\Livewire\\Importer',
+  'login-form' => 'App\\Http\\Livewire\\LoginForm',
+  'slack-settings-form' => 'App\\Http\\Livewire\\SlackSettingsForm',
+);
---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,194 @@
+<?php return array (
+  'arietimmerman/laravel-scim-server' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'ArieTimmerman\\Laravel\\SCIMServer\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'SCIMServerHelper' => 'ArieTimmerman\\Laravel\\SCIMServer\\Helper',
+    ),
+  ),
+  'barryvdh/laravel-debugbar' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\Debugbar\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Debugbar' => 'Barryvdh\\Debugbar\\Facades\\Debugbar',
+    ),
+  ),
+  'barryvdh/laravel-dompdf' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\DomPDF\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Pdf' => 'Barryvdh\\DomPDF\\Facade\\Pdf',
+      'PDF' => 'Barryvdh\\DomPDF\\Facade\\Pdf',
+    ),
+  ),
+  'eduardokum/laravel-mail-auto-embed' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Eduardokum\\LaravelMailAutoEmbed\\ServiceProvider',
+    ),
+  ),
+  'facade/ignition' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Facade\\Ignition\\IgnitionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Flare' => 'Facade\\Ignition\\Facades\\Flare',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'fruitcake/laravel-cors' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fruitcake\\Cors\\CorsServiceProvider',
+    ),
+  ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+  'laravel-notification-channels/google-chat' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NotificationChannels\\GoogleChat\\GoogleChatServiceProvider',
+    ),
+  ),
+  'laravel-notification-channels/microsoft-teams' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NotificationChannels\\MicrosoftTeams\\MicrosoftTeamsServiceProvider',
+    ),
+  ),
+  'laravel/passport' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Passport\\PassportServiceProvider',
+    ),
+  ),
+  'laravel/slack-notification-channel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Illuminate\\Notifications\\SlackChannelServiceProvider',
+    ),
+  ),
+  'laravel/socialite' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Socialite\\SocialiteServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Socialite' => 'Laravel\\Socialite\\Facades\\Socialite',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'laravel/ui' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Ui\\UiServiceProvider',
+    ),
+  ),
+  'laravelcollective/html' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Collective\\Html\\HtmlServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Form' => 'Collective\\Html\\FormFacade',
+      'Html' => 'Collective\\Html\\HtmlFacade',
+    ),
+  ),
+  'livewire/livewire' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Livewire\\LivewireServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Livewire' => 'Livewire\\Livewire',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'pragmarx/google2fa-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'PragmaRX\\Google2FALaravel\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Google2FA' => 'PragmaRX\\Google2FALaravel\\Facade',
+    ),
+  ),
+  'spatie/laravel-backup' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Backup\\BackupServiceProvider',
+    ),
+  ),
+  'unicodeveloper/laravel-password' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Unicodeveloper\\DumbPassword\\DumbPasswordServiceProvider',
+    ),
+  ),
+);
